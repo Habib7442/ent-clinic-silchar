adf223790eaaedbb698472dd9f91fe234cec37aa
Habib Tanwir <habibtanwir1906@gmail.com>
2026-05-20 11:59:31 +0530

    feat: remove placeholder credentials from the About page

--- app/[lang]/about/page.tsx
+++ app/[lang]/about/page.tsx
@@ -1,6 +1,5 @@
 import DoctorHero from "@/components/editorial/DoctorHero";
 import DoctorPhilosophy from "@/components/editorial/DoctorPhilosophy";
-import DoctorCredentials from "@/components/editorial/DoctorCredentials";
 import ClosingCTA from "@/components/editorial/ClosingCTA";
 
 export default async function AboutPage({
@@ -14,7 +13,6 @@ export default async function AboutPage({
     <main className="flex-1 overflow-x-hidden">
       <DoctorHero lang={lang} />
       <DoctorPhilosophy lang={lang} />
-      <DoctorCredentials lang={lang} />
       <ClosingCTA lang={lang} />
     </main>
   );
